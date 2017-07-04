50971a578f87404aa1881e20744f75938d64a9d4
JellyLu <634725546@qq.com>
2017-07-04 23:15:33 +0800

    use redux to manage the data in todo app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 'use strict';
 const React = require('react');
-const {Text, NavigatorIOS} = require('react-native');
+const {Provider} = require('react-redux');
+const store = require('./stores/todoAppStore');
+const {NavigatorIOS} = require('react-native');
 const styles = require('./styles/shareStyles');
-const ToDoListContainer = require ('./component/todoListContainer');
+const TodoApp = require ('./pages/todoApp');
 
-class Setup  extends React.Component {
-  render() {
-    return (
-      <NavigatorIOS
-      style={styles.navigator}
-      initialRoute={{component: ToDoListContainer, title: 'ToDo List - J'}}
-      />
-    );
-  }
+class Setup extends React.Component {
+    render() {
+      return (
+        <Provider store={store}>
+          <NavigatorIOS style={styles.navigator}
+          initialRoute={{component: TodoApp, title: 'ToDo List - J'}}
+          />
+        </Provider>
+      );
+    }
 }
+
+
+global.LOG = (...args) => {
+  console.log('/------------------------------\\');
+  console.log(...args);
+  console.log('\\------------------------------/');
+  return args[args.length - 1];
+};
 
 module.exports = Setup;
