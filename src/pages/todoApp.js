8e09fe13adc06aa702a157cd0c14cac4c8eca977
JellyLu <634725546@qq.com>
2017-07-04 23:14:58 +0800

    add todo list page

---
+++ src/pages/todoApp.js
@@ -0,0 +1,23 @@
+const {connect} = require('react-redux');
+const {VisibilityFilters} = require('../constants/constants');
+const ToDoListContainer = require ('../component/todoListContainer');
+
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed);
+  }
+}
+
+function select(state) {
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  };
+}
+
+module.exports = connect(select)(ToDoListContainer);
